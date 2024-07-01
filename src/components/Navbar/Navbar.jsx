d47a97675ed55041a179273c9ee1808b55327ef3
Deepanraj K <deepanraj770848@gmail.com>
2024-07-01 17:43:24 +0530

    add

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../Assets/logo.png'
+
+const Navbar = () => {
+    return (
+        <div className='navbar-main'>
+            <div className='navbar'>
+            <ul className="nav-menu-left">
+                <li className="nav-item">Home</li>
+                <li className="nav-item">About</li>
+                <li className="nav-item">Service</li>
+            </ul>
+            <div className="nav-item nav-logo">
+                <img src={logo} alt="" />
+                <div>
+                    <p className='nav-name-prev'>DR</p>
+                    <p className='nav-name-full'>Made by <br /><span>Deepanraj</span></p>
+                </div>
+            </div>
+            <ul className="nav-menu-right">
+                <li className="nav-item">Home</li>
+                <li className="nav-item">About</li>
+                <li className="nav-item">Service</li>
+            </ul>
+
+        </div>
+        </div>
+        
+    )
+}
+
+export default Navbar
